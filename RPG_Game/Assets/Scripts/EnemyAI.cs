d503aebef497362c040d9c1e5c1bdbb4f44201d4
thaonee0 <phuongthaopham4814@gmail.com>
2024-05-20 23:44:03 +0700

    add enemy - blue slime

---
+++ RPG_Game/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum State
+    {
+        Roaming
+    }
+
+    private State state;
+    private EnemyPathFinding enemyPathFinding;
+
+    private void Awake()
+    {
+        enemyPathFinding = GetComponent<EnemyPathFinding>();
+        state = State.Roaming;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+
+    private IEnumerator RoamingRoutine()
+    {
+        while (state == State.Roaming)
+        {
+            Vector2 roamPosision = GetRoamingPosition();
+            enemyPathFinding.MoveTo(roamPosision);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private Vector2 GetRoamingPosition()
+    {
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+}
