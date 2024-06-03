5e7313802217d651b4c7a219ff5decfaa6bad36d
thaonee0 <phuongthaopham4814@gmail.com>
2024-06-03 22:37:52 +0700

    camera controller

--- RPG_Game/Assets/Scripts/Singleton.cs
+++ RPG_Game/Assets/Scripts/Singleton.cs
@@ -23,6 +23,9 @@ public class Singleton<T> : MonoBehaviour where T : Singleton<T>
             instance = (T)this;
         }
 
-        DontDestroyOnLoad(gameObject);
+        if (!gameObject.transform.parent)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
