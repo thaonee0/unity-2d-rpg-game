797812370c51afab946305c441dfe5f88f13ff5c
kiyari005 <letuankiet005@gmail.com>
2024-06-24 17:30:37 +0700

    tuan kiet

---
+++ RPG_Game/Assets/Scripts/Boss/Boss Manager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireRate = 1f;
+    private float nextFireTime;
+
+    private Transform playerTransform; // Tham chiếu đến transform của người chơi
+
+    void Start()
+    {
+        // Tìm đối tượng người chơi bằng tag (cần đặt tag cho đối tượng người chơi là "Player")
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        // Cập nhật vị trí của người chơi
+        UpdatePlayerPosition();
+
+        if (Time.time >= nextFireTime)
+        {
+            Attack();
+            nextFireTime = Time.time + 1f / fireRate;
+        }
+    }
+
+    void UpdatePlayerPosition()
+    {
+        // Kiểm tra nếu không tìm thấy người chơi
+        if (playerTransform == null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+            {
+                playerTransform = playerObject.transform;
+            }
+        }
+        else
+        {
+            // Cập nhật vị trí của người chơi
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    void Attack()
+    {
+        // Kiểm tra nếu chưa có người chơi
+        if (playerTransform == null)
+        {
+            return;
+        }
+
+        // Hướng đạn đến vị trí của người chơi
+        Vector2 fireDirection = (playerTransform.position - firePoint.position).normalized;
+
+        // Tạo viên đạn và hướng nó
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        bullet.transform.right = fireDirection;
+
+    }
+
+}
--- RPG_Game/Assets/Scripts/Management/Singleton.cs
+++ RPG_Game/Assets/Scripts/Management/Singleton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
-    private static T instance;
+    protected static T instance;
     public static T Instance
     { 
         get 
